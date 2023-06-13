f54ba1e2deca878f43ec526a266ae403d82ebb63
Petr Kasalicky <kasalpe1@fit.cvut.cz>
2023-06-13 09:50:54 +0800

    Apply black and make it PEP8, update requirements

--- code/register.py
+++ code/register.py
@@ -1,15 +1,14 @@
 import world
 import dataloader
 import model
-import utils
 from pprint import pprint
 
-if world.dataset in ['gowalla', 'yelp2018', 'amazon-book']:
-    dataset = dataloader.Loader(path="../data/"+world.dataset)
-elif world.dataset == 'lastfm':
+if world.dataset in ["gowalla", "yelp2018", "amazon-book"]:
+    dataset = dataloader.Loader(path="../data/" + world.dataset)
+elif world.dataset == "lastfm":
     dataset = dataloader.LastFM()
 
-print('===========config================')
+print("===========config================")
 pprint(world.config)
 print("cores for test:", world.CORES)
 print("comment:", world.comment)
@@ -18,9 +17,6 @@ print("LOAD:", world.LOAD)
 print("Weight path:", world.PATH)
 print("Test Topks:", world.topks)
 print("using bpr loss")
-print('===========end===================')
+print("===========end===================")
 
-MODELS = {
-    'mf': model.PureMF,
-    'lgn': model.LightGCN
-}
+MODELS = {"mf": model.PureMF, "lgn": model.LightGCN}
