8498a24466a9e20491cf5b1066eee4285adc4803
Petr Kasalicky <kasalpe1@fit.cvut.cz>
2023-06-15 18:27:49 +0800

    Add draft with working training

--- code/main.py
+++ code/main.py
@@ -6,9 +6,8 @@ import torch
 from torch.utils.tensorboard import SummaryWriter
 
 import dataloader
-import model
+from model import LightGCN, UniformSamplingDataset, SaveMetricsCallback
 import utils
-import procedure
 from parse import parse_args
 
 LOGGING_FORMAT = "[%(asctime)s] %(levelname)s: %(message)s"
@@ -56,134 +55,144 @@ elif args.dataset == "lastfm":
 else:
     raise NotImplementedError(f"Unsupported dataset {args.dataset}")
 
-rec_model = model.LightGCN(args.layers, args.latent_dim, dataset=dataset)
-rec_model = rec_model.to(DEVICE)
-bpr = utils.BPRLoss(rec_model, args.lambda_, args.lr)
+model = LightGCN(
+    args.layers, args.latent_dim, n_users=dataset.n_users,
+    n_items=dataset.m_items, training_graph=dataset.get_sparse_graph(), device=DEVICE,
+    learning_rate=args.lr, lambda_=args.lambda_
+)
 
-weight_file = os.path.join(FILE_PATH, f"lgn-{args.dataset}-{args.layers}-{args.latent_dim}.pth.tar")
+train_dataloader = torch.utils.data.DataLoader(
+    UniformSamplingDataset(dataset),
+    shuffle=True,
+    batch_size=args.train_batch
+)
 
-logging.info(f"Weights will be saved to {weight_file}")
-if args.load_previous:
-    try:
-        logging.info(f"Flag for loading previous weights was trigged, loading model with weights from {weight_file}")
-        rec_model.load_state_dict(torch.load(weight_file, map_location=torch.device("cpu")))
-    except FileNotFoundError:
-        logging.warning(f"File {weight_file} does not exist, model will be trained from beginning")
+# weight_file = os.path.join(FILE_PATH, f"lgn-{args.dataset}-{args.layers}-{args.latent_dim}.pth.tar")
+#
+# logging.info(f"Weights will be saved to {weight_file}")
+# if args.load_previous:
+#     try:
+#         logging.info(f"Flag for loading previous weights was trigged, loading model with weights from {weight_file}")
+#         rec_model.load_state_dict(torch.load(weight_file, map_location=torch.device("cpu")))
+#     except FileNotFoundError:
+#         logging.warning(f"File {weight_file} does not exist, model will be trained from beginning")
 
 # init tensorboard
 if args.tensorboard:
     path = os.path.join(BOARD_PATH, time.strftime("%m-%d-%Hh%Mm%Ss-") + "LGN")
     writer = SummaryWriter(path)
     logging.info(f"Tensorboard was inicitialized to path={path}")
-
 else:
     writer = None
     logging.info("Tensorboard will not be used")
 
-try:
-    for epoch in range(1, args.epochs + 1):
-        rec_model.train()
-        results = procedure.BPR_train_original(dataset, args.train_batch, DEVICE, bpr, epoch, writer=writer)
-        logging.info(f"[TRAIN] Epoch: {epoch}/{args.epochs}; {utils.results_to_progress_log(results)}")
-        if epoch % 1 == 0:
-            results = procedure.test(dataset, rec_model, args.topks, args.test_batch, DEVICE, args.n_threads, epoch, writer)
-            logging.info(f"[TEST] Epoch: {epoch}/{args.epochs}; {utils.results_to_progress_log(results)}")
-            if epoch == 1:
-                assert round(results["Recall@20"], 4) == round(0.08642416, 4)
-                assert round(results["Precision@20"], 4) == round(0.02703128, 4)
-                assert round(results["NDCG@20"], 4) == round(0.07227541, 4)
+train_callbacks = [SaveMetricsCallback(subset_name="train", writer=writer)]
+model.fit(train_dataloader, epochs=10, train_callbacks=train_callbacks)
 
-            if epoch == 2:
-                assert round(results["Recall@20"], 4) == round(0.09613762, 4)
-                assert round(results["Precision@20"], 4) == round(0.03018119, 4)
-                assert round(results["NDCG@20"], 4) == round(0.08025454, 4)
-
-            if epoch == 3:
-                assert round(results["Recall@20"], 4) == round(0.10085652, 4)
-                assert round(results["Precision@20"], 4) == round(0.03160627, 4)
-                assert round(results["NDCG@20"], 4) == round(0.08451248, 4)
-
-            if epoch == 4:
-                assert round(results["Recall@20"], 4) == round(0.10535001, 4)
-                assert round(results["Precision@20"], 4) == round(0.0328103, 4)
-                assert round(results["NDCG@20"], 4) == round(0.08842302, 4)
-
-            if epoch == 5:
-                assert round(results["Recall@20"], 4) == round(0.10873527, 4)
-                assert round(results["Precision@20"], 4) == round(0.03369951, 4)
-                assert round(results["NDCG@20"], 4) == round(0.0915894, 4)
-
-            if epoch == 6:
-                assert round(results["Recall@20"], 4) == round(0.11204279, 4)
-                assert round(results["Precision@20"], 4) == round(0.0345636, 4)
-                assert round(results["NDCG@20"], 4) == round(0.09433213, 4)
-
-            if epoch == 7:
-                assert round(results["Recall@20"], 4) == round(0.1144635, 4)
-                assert round(results["Precision@20"], 4) == round(0.03518655, 4)
-                assert round(results["NDCG@20"], 4) == round(0.09638503, 4)
-
-            if epoch == 8:
-                assert round(results["Recall@20"], 4) == round(0.1166778, 4)
-                assert round(results["Precision@20"], 4) == round(0.03579945, 4)
-                assert round(results["NDCG@20"], 4) == round(0.09808936, 4)
-
-            if epoch == 9:
-                assert round(results["Recall@20"], 4) == round(0.11843803, 4)
-                assert round(results["Precision@20"], 4) == round(0.03622815, 4)
-                assert round(results["NDCG@20"], 4) == round(0.09946748, 4)
-
-            if epoch == 10:
-                assert round(results["Recall@20"], 4) == round(0.12013993, 4)
-                assert round(results["Precision@20"], 4) == round(0.03667024, 4)
-                assert round(results["NDCG@20"], 4) == round(0.10055132, 4)
-
-            if epoch == 11:
-                assert round(results["Recall@20"], 4) == round(0.12134588, 4)
-                assert round(results["Precision@20"], 4) == round(0.03699344, 4)
-                assert round(results["NDCG@20"], 4) == round(0.10154486, 4)
-
-            if epoch == 12:
-                assert round(results["Recall@20"], 4) == round(0.12283394, 4)
-                assert round(results["Precision@20"], 4) == round(0.03736352, 4)
-                assert round(results["NDCG@20"], 4) == round(0.10243225, 4)
-
-            if epoch == 13:
-                assert round(results["Recall@20"], 4) == round(0.12388475, 4)
-                assert round(results["Precision@20"], 4) == round(0.0376415, 4)
-                assert round(results["NDCG@20"], 4) == round(0.10322438, 4)
-
-            if epoch == 14:
-                assert round(results["Recall@20"], 4) == round(0.1251952, 4)
-                assert round(results["Precision@20"], 4) == round(0.03798814, 4)
-                assert round(results["NDCG@20"], 4) == round(0.10429674, 4)
-
-            if epoch == 15:
-                assert round(results["Recall@20"], 4) == round(0.12649249, 4)
-                assert round(results["Precision@20"], 4) == round(0.03834148, 4)
-                assert round(results["NDCG@20"], 4) == round(0.10514867, 4)
-
-            if epoch == 16:
-                assert round(results["Recall@20"], 4) == round(0.12744638, 4)
-                assert round(results["Precision@20"], 4) == round(0.03861612, 4)
-                assert round(results["NDCG@20"], 4) == round(0.1058937, 4)
-
-            if epoch == 17:
-                assert round(results["Recall@20"], 4) == round(0.12834675, 4)
-                assert round(results["Precision@20"], 4) == round(0.03886563, 4)
-                assert round(results["NDCG@20"], 4) == round(0.10649789, 4)
-
-            if epoch == 18:
-                assert round(results["Recall@20"], 4) == round(0.12956465, 4)
-                assert round(results["Precision@20"], 4) == round(0.03916203, 4)
-                assert round(results["NDCG@20"], 4) == round(0.10737581, 4)
-
-            if epoch == 19:
-                assert round(results["Recall@20"], 4) == round(0.1303976, 4)
-                assert round(results["Precision@20"], 4) == round(0.03941155, 4)
-                assert round(results["NDCG@20"], 4) == round(0.10797482, 4)
-
-        torch.save(rec_model.state_dict(), weight_file)
-finally:
-    if writer is not None:
-        writer.close()
+# try:
+#     for epoch in range(1, args.epochs + 1):
+#         rec_model.train()
+#         results = procedure.BPR_train_original(dataset, args.train_batch, DEVICE, bpr, epoch, writer=writer)
+#         logging.info(f"[TRAIN] Epoch: {epoch}/{args.epochs}; {utils.results_to_progress_log(results)}")
+#         if epoch % 1 == 0:
+#             results = procedure.test(dataset, rec_model, args.topks, args.test_batch, DEVICE, args.n_threads, epoch, writer)
+#             logging.info(f"[TEST] Epoch: {epoch}/{args.epochs}; {utils.results_to_progress_log(results)}")
+#             if epoch == 1:
+#                 assert round(results["Recall@20"], 4) == round(0.08642416, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.02703128, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.07227541, 4)
+# 
+#             if epoch == 2:
+#                 assert round(results["Recall@20"], 4) == round(0.09613762, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.03018119, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.08025454, 4)
+# 
+#             if epoch == 3:
+#                 assert round(results["Recall@20"], 4) == round(0.10085652, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.03160627, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.08451248, 4)
+# 
+#             if epoch == 4:
+#                 assert round(results["Recall@20"], 4) == round(0.10535001, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.0328103, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.08842302, 4)
+# 
+#             if epoch == 5:
+#                 assert round(results["Recall@20"], 4) == round(0.10873527, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.03369951, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.0915894, 4)
+# 
+#             if epoch == 6:
+#                 assert round(results["Recall@20"], 4) == round(0.11204279, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.0345636, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.09433213, 4)
+# 
+#             if epoch == 7:
+#                 assert round(results["Recall@20"], 4) == round(0.1144635, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.03518655, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.09638503, 4)
+# 
+#             if epoch == 8:
+#                 assert round(results["Recall@20"], 4) == round(0.1166778, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.03579945, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.09808936, 4)
+# 
+#             if epoch == 9:
+#                 assert round(results["Recall@20"], 4) == round(0.11843803, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.03622815, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.09946748, 4)
+# 
+#             if epoch == 10:
+#                 assert round(results["Recall@20"], 4) == round(0.12013993, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.03667024, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.10055132, 4)
+# 
+#             if epoch == 11:
+#                 assert round(results["Recall@20"], 4) == round(0.12134588, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.03699344, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.10154486, 4)
+# 
+#             if epoch == 12:
+#                 assert round(results["Recall@20"], 4) == round(0.12283394, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.03736352, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.10243225, 4)
+# 
+#             if epoch == 13:
+#                 assert round(results["Recall@20"], 4) == round(0.12388475, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.0376415, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.10322438, 4)
+# 
+#             if epoch == 14:
+#                 assert round(results["Recall@20"], 4) == round(0.1251952, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.03798814, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.10429674, 4)
+# 
+#             if epoch == 15:
+#                 assert round(results["Recall@20"], 4) == round(0.12649249, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.03834148, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.10514867, 4)
+# 
+#             if epoch == 16:
+#                 assert round(results["Recall@20"], 4) == round(0.12744638, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.03861612, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.1058937, 4)
+# 
+#             if epoch == 17:
+#                 assert round(results["Recall@20"], 4) == round(0.12834675, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.03886563, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.10649789, 4)
+# 
+#             if epoch == 18:
+#                 assert round(results["Recall@20"], 4) == round(0.12956465, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.03916203, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.10737581, 4)
+# 
+#             if epoch == 19:
+#                 assert round(results["Recall@20"], 4) == round(0.1303976, 4)
+#                 assert round(results["Precision@20"], 4) == round(0.03941155, 4)
+#                 assert round(results["NDCG@20"], 4) == round(0.10797482, 4)
+# 
+#         torch.save(rec_model.state_dict(), weight_file)
+# finally:
+#     if writer is not None:
+#         writer.close()
--- code/model.py
+++ code/model.py
@@ -8,66 +8,153 @@ Xiangnan He et al. LightGCN: Simplifying and Powering Graph Convolution Network 
 Define models here
 """
 import logging
+import os
+import time
 
 import torch
+import numpy as np
+from torch.utils.tensorboard import SummaryWriter
 
 from dataloader import BasicDataset
 
 
 class LightGCN(torch.nn.Module):
-    def __init__(self, n_layers: int, latent_dim: int, dataset: BasicDataset):
+    def __init__(self, n_layers: int, latent_dim: int, n_users: int, n_items: int, training_graph: torch.Tensor, learning_rate: float, device: torch.device, lambda_: float):
         super(LightGCN, self).__init__()
         self.latent_dim = latent_dim
         self.n_layers = n_layers
-        self.dataset = dataset
-        self.num_users = self.dataset.n_users
-        self.num_items = self.dataset.m_items
-        self.embedding_user = torch.nn.Embedding(num_embeddings=self.num_users, embedding_dim=self.latent_dim)
-        self.embedding_item = torch.nn.Embedding(num_embeddings=self.num_items, embedding_dim=self.latent_dim)
+        self.__n_users = n_users
+        self.__n_items = n_items
+        self.__lambda_ = lambda_
+        self.__embedding_user = torch.nn.Embedding(num_embeddings=self.__n_users, embedding_dim=self.latent_dim)
+        self.__embedding_item = torch.nn.Embedding(num_embeddings=self.__n_items, embedding_dim=self.latent_dim)
         # random normal init seems to be a better choice when lightGCN actually don't use any non-linear activation function
-        torch.nn.init.normal_(self.embedding_user.weight, std=0.1)
-        torch.nn.init.normal_(self.embedding_item.weight, std=0.1)
-        self.f = torch.nn.Sigmoid()
-        self.training_graph = self.dataset.get_sparse_graph()
+        torch.nn.init.normal_(self.__embedding_user.weight, std=0.1)
+        torch.nn.init.normal_(self.__embedding_item.weight, std=0.1)
+        self.__training_graph = training_graph
+        self.__optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
+        self.__device = device
+        self.to(device)
         logging.info("LightGCN model was successfully initialized")
 
-    def computer(self):
+    def fit(
+        self,
+        train_data: torch.utils.data.DataLoader,
+        epochs: int,
+        train_callbacks: list = [],
+        validation_data: torch.utils.data.DataLoader = None,
+        validation_callbacks: list = [],
+        test_data: torch.utils.data.DataLoader = None,
+        test_callbacks: list = [],
+        savedir: str = None,
+        print_interval: int = None,
+    ):
+        total_steps = len(train_data)
+        if print_interval is None:
+            print_interval = total_steps
+
+        logging.info("************************** [START] **************************")
+        logging.info(f"Runing on {self.__device}.")
+        logging.info("Total steps {:n}".format(total_steps))
+
+        for current_epoch in range(1, epochs + 1):
+            for callback in train_callbacks:
+                callback.start_epoch(current_epoch, epochs, total_steps)
+            for step, (user_indices, positive_item_indices, negative_item_indices) in enumerate(train_data, start=1):
+                # for callback in train_callbacks:
+                #     if isinstance(callback, SavePredictionsCallback):
+                #         callback(user_indices=user_indices, item_indices=item_indices, target_ratings=target_ratings)
+                self.train_step(user_indices, positive_item_indices, negative_item_indices, callbacks=train_callbacks)
+                if step % print_interval == 0 or step == total_steps:
+                    for callback in train_callbacks:
+                        if isinstance(callback, SaveMetricsCallback):
+                            callback.print_metrics()
+
+            if validation_data is not None:
+                # for callback in validation_callbacks:
+                #     callback.start_epoch(current_epoch, epochs, len(validation_data))
+                self.eval_data(validation_data, callbacks=validation_callbacks)
+
+            if test_data is not None:
+                # for callback in test_callbacks:
+                #     callback.start_epoch(current_epoch, epochs, len(test_data))
+                self.eval_data(test_data, callbacks=test_callbacks)
+
+            if savedir is not None:
+                torch.save(
+                    self.state_dict(),
+                    os.path.join(savedir, f"epoch-{current_epoch}.pth"),
+                )
+            # for callback in train_callbacks:
+            #     callback.end_epoch()
+        logging.info("************************** [END] **************************")
+
+    def train_step(self, user_indices: torch.Tensor, positive_item_indices: torch.Tensor, negative_item_indices: torch.Tensor, callbacks: list):
+        self.train()
+        self.zero_grad()
+        user_indices.to(self.__device)
+        positive_item_indices.to(self.__device)
+        negative_item_indices.to(self.__device)
+        bpr_loss, reg_loss = self.__bpr_loss(user_indices, positive_item_indices, negative_item_indices)
+        loss = bpr_loss + reg_loss * self.__lambda_
+        for callback in callbacks:
+            if isinstance(callback, SaveMetricsCallback):
+                callback(bpr_loss=loss, reg_loss=reg_loss, loss=loss)
+            else:
+                raise NotImplementedError(f"Unsupported callback with type {type(callback)}")
+        loss.backward()
+        self.__optimizer.step()
+        torch.cuda.empty_cache()
+
+    # def eval_data(self, dataloader: torch.utils.data.DataLoader, callbacks: list):
+    #     self.eval()
+    #     with torch.no_grad():
+    #         for graphs, user_indices, item_indices, target_ratings in dataloader:
+    #             # for callback in callbacks:
+    #             #     if isinstance(callback, SavePredictionsCallback):
+    #             #         callback(user_indices=user_indices, item_indices=item_indices, target_ratings=target_ratings)
+    #             self.predict_and_compute_loss(graphs, callbacks)
+    #             torch.cuda.empty_cache()
+    #     # for callback in callbacks:
+    #     #     if isinstance(callback, SaveMetricsCallback):
+    #     #         callback.print_metrics()
+    #     #     callback.end_epoch()
+
+    def __compute_embeddings(self):
         """
         Compute embeddings for all items and users
         """
-        users_emb = self.embedding_user.weight
-        items_emb = self.embedding_item.weight
+        users_emb = self.__embedding_user.weight
+        items_emb = self.__embedding_item.weight
         all_emb = torch.cat([users_emb, items_emb])
         embeddings_per_layer = [all_emb]
-        g = self.training_graph
         for layer in range(self.n_layers):
-            all_emb = torch.sparse.mm(g, all_emb)
+            all_emb = torch.sparse.mm(self.__training_graph, all_emb)
             embeddings_per_layer.append(all_emb)
         embeddings_per_layer = torch.stack(embeddings_per_layer, dim=1)
         light_out = torch.mean(embeddings_per_layer, dim=1)
-        users, items = torch.split(light_out, [self.num_users, self.num_items])
+        users, items = torch.split(light_out, [self.__n_users, self.__n_items])
         return users, items
 
     def predict_users_rating(self, users: torch.Tensor):
-        all_users, all_items = self.computer()
-        users_emb = all_users[users]
-        items_emb = all_items
-        rating = self.f(users_emb @ items_emb.T)
+        users_emb_all, items_emb = self.__compute_embeddings()
+        users_emb = users_emb_all[users]
+        rating = torch.sigmoid(users_emb @ items_emb.T)
         return rating
 
-    def get_embedding(self, users: torch.Tensor, pos_items: torch.Tensor, neg_items: torch.Tensor):
-        all_users, all_items = self.computer()
+    def __get_embedding(self, users: torch.Tensor, pos_items: torch.Tensor, neg_items: torch.Tensor):
+        all_users, all_items = self.__compute_embeddings()
         users_emb = all_users[users]
         pos_emb = all_items[pos_items]
         neg_emb = all_items[neg_items]
-        users_emb_ego = self.embedding_user(users)
-        pos_emb_ego = self.embedding_item(pos_items)
-        neg_emb_ego = self.embedding_item(neg_items)
+        users_emb_ego = self.__embedding_user(users)
+        pos_emb_ego = self.__embedding_item(pos_items)
+        neg_emb_ego = self.__embedding_item(neg_items)
         return users_emb, pos_emb, neg_emb, users_emb_ego, pos_emb_ego, neg_emb_ego
 
-    def bpr_loss(self, users: torch.Tensor, pos: torch.Tensor, neg: torch.Tensor):
+    def __bpr_loss(self, users: torch.Tensor, pos: torch.Tensor, neg: torch.Tensor):
         # Bayesian Personalized Ranking
-        (users_emb, pos_emb, neg_emb, userEmb0, posEmb0, negEmb0) = self.get_embedding(users, pos, neg)
+        (users_emb, pos_emb, neg_emb, userEmb0, posEmb0, negEmb0) = self.__get_embedding(users, pos, neg)
         reg_loss = (1 / 2) * (userEmb0.norm(2).pow(2) + posEmb0.norm(2).pow(2) + negEmb0.norm(2).pow(2)) / float(len(users))
         pos_scores = torch.mul(users_emb, pos_emb)
         pos_scores = torch.sum(pos_scores, dim=1)
@@ -77,3 +164,111 @@ class LightGCN(torch.nn.Module):
         loss = torch.mean(torch.nn.functional.softplus(neg_scores - pos_scores))
 
         return loss, reg_loss
+
+
+class UniformSamplingDataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        dataset: BasicDataset
+    ):
+        super(UniformSamplingDataset, self).__init__()
+        self.dataset = dataset
+
+    def __len__(self):
+        """
+        Returns the number of samples in the dataset.
+        """
+        return self.dataset.n_users
+
+    def len(self):
+        return self.__len__()
+
+    def __getitem__(self, idx):
+        """
+        For each user, take only one randomly selected item interacted by the users as positive item
+        and one item not-interacted by the user as negative item
+        """
+        user_index = idx
+        interacted_items = self.dataset.get_user_pos_items([user_index])[0]
+        positive_item_index = np.random.choice(interacted_items)
+        while True:
+            negative_item_index = np.random.randint(0, self.dataset.m_items)
+            # this loop will run be broken if user interacted with all items
+            if negative_item_index in interacted_items:
+                continue
+            else:
+                break
+        return user_index, positive_item_index, negative_item_index
+
+
+class SaveMetricsCallback:
+    BPR_LOSS_ATTR = "bpr_loss"
+    REGULARIZATION_LOSS_ATTR = "reg_loss"
+    LOSS_ATTR = "loss"
+
+    def __init__(self, subset_name: str, writer: SummaryWriter = None):
+        self.subset_name = subset_name
+        self.writer = writer
+        self.dynamic_attributes = set()
+        self.__reset()
+        # Attributes that can be added by '__call__' method and they are deleted in 'end_epoch' method
+        self.supported_keys = [self.BPR_LOSS_ATTR, self.REGULARIZATION_LOSS_ATTR, self.LOSS_ATTR]
+
+    def start_epoch(self, current_epoch: int, total_epochs: int, total_steps: int):
+        self.start_time = time.time()
+        self.current_epoch = current_epoch
+        self.total_epochs = total_epochs
+        self.current_step = 0
+        self.total_steps = total_steps
+
+    def print_metrics(self):
+        elapsed_time = time.time() - self.start_time
+        log_dict = {
+            "Epoch": f"{self.current_epoch}/{self.total_epochs}",
+            "Step": f"{self.current_step}/{self.total_steps}",
+            "Time": f"{elapsed_time:2f}s",
+            "Subset": self.subset_name,
+        }
+        log_dict.update(self.__aggregate_metrics())
+        logging.info(self.get_progress_log(log_dict))
+
+    def __aggregate_metrics(self):
+        metrics = {}
+        for param_to_agg in self.supported_keys:
+            val = getattr(self, param_to_agg)
+            metrics[f"{param_to_agg}"] = torch.mean(torch.Tensor(val)).item()
+        return metrics
+
+    def __call__(self, **kwargs):
+        self.current_step += 1
+        for key, val in kwargs.items():
+            if key not in self.supported_keys:
+                raise ValueError(f"{type(self).__name__}: Key '{key}' is not listed in the supported keys {self.supported_keys}")
+            self.dynamic_attributes.add(key)
+            if isinstance(val, torch.Tensor):
+                val = val.detach().cpu()
+            if hasattr(self, key):
+                getattr(self, key).append(val)
+            else:
+                setattr(self, key, [val])
+
+    def __reset(self):
+        self.current_step = 0
+        for attr in self.dynamic_attributes:
+            delattr(self, attr)
+        self.dynamic_attributes.clear()
+
+    def end_epoch(self):
+        if self.writer is not None:
+            for metric_name, value in self.__aggregate_metrics().items():
+                self.writer.add_scalar(f"{metric_name}/{self.subset_name}", value, self.current_epoch)
+        self.__reset()
+
+    @staticmethod
+    def get_progress_log(values: dict, delimiter: str = "; "):
+        def format_number(v):
+            if isinstance(v, float):
+                return str(round(v, 4))
+            return str(v)
+
+        return delimiter.join(f"{key}: {format_number(val)}" for key, val in values.items())
